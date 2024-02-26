e53462edddf3278c47ba1d2a59d26f48f66e3702
Asim Mutlu <asimmutlu6134@gmail.com>
2024-02-26 16:01:26 +0300

    AdminHome Paged added and more.

---
+++ FoodyTekmerWebUI/Controllers/AdminHomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodyTekmerWebUI.Controllers
+{
+    public class AdminHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- FoodyTekmerWebUI/Program.cs
+++ FoodyTekmerWebUI/Program.cs
@@ -46,6 +46,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=AdminHome}/{action=Index}/{id?}");
 
 app.Run();
