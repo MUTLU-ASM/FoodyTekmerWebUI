fa95646a3ba575628571c824e5327503c9df4e47
Asim Mutlu <asimmutlu6134@gmail.com>
2024-02-28 12:40:37 +0300

    AdminHome page viewbag added.

--- FoodyTekmerWebUI/Controllers/AdminHomeController.cs
+++ FoodyTekmerWebUI/Controllers/AdminHomeController.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FoodyTekmerDataAccessLayer.Context;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FoodyTekmerWebUI.Controllers
 {
     public class AdminHomeController : Controller
     {
+        FoodyContext context = new();
+
         public IActionResult Index()
         {
+            ViewBag.product = context.Products.Count();
+            ViewBag.category = context.Categories.Count();
+            ViewBag.testimonial = context.Testimonials.Count();
+            ViewBag.contact = context.Contacts.Count();
             return View();
         }
     }
