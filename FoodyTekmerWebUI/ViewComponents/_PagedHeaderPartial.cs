b7295c94f36a32b089331831237e2666a8d5b75b
Asim Mutlu <asimmutlu6134@gmail.com>
2024-03-16 14:08:32 +0300

    PagedHeader Partial edited.

---
+++ FoodyTekmerWebUI/ViewComponents/_PagedHeaderPartial.cs
@@ -0,0 +1,23 @@
+﻿using FoodyTekmerEntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodyTekmerWebUI.ViewComponents
+{
+    public class _PagedHeaderPartial:ViewComponent
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public _PagedHeaderPartial(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.userName = values.Name + " " + values.Surname;
+            ViewBag.userImg = values.ImageUrl;
+            return View();
+        }
+    }
+}
